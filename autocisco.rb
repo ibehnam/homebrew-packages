75f296aaae7e96ccb8b1ff10ce1b8649c0087234
Behnam Moh <58621210+ibehnam@users.noreply.github.com>
2024-02-22 22:50:50 -0500

    version bump

--- autocisco.rb
+++ autocisco.rb
@@ -4,9 +4,9 @@
 class Autocisco < Formula
   desc "Automatically connects to University VPN (currently CMU's Cisco VPN) using your credentials"
   homepage "https://aplaceofmind.notion.site/AutoCisco-My-First-Project-in-Rust-c40c831d5eb0447ea9246c0514558195?pvs=74"
-  url "https://github.com/ibehnam/AutoCisco/releases/download/stable-0.1.4/autocisco-mac.tar.gz"
-  sha256 "5b9b4b4cb588de6e9003232d36d4f11b880bc5b7b9b43f245a5d2d9f3c1c15f0"
-  version "0.1.4"
+  url "https://github.com/ibehnam/AutoCisco/releases/download/stable-0.1.5/autocisco-mac.tar.gz"
+  sha256 "31a749b087300e985573d5722daab694b37d8924dcf0be789071fa041def6093"
+  version "0.1.5"
 
   def install
     bin.install "autocisco"
