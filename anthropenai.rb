3f7838145b2ac205eb24f2f7ac2f1533c6d1ae93
Behnam M <58621210+ibehnam@users.noreply.github.com>
2026-01-16 00:59:54 -0600

    Update anthropenai to use git URL for private repo support

--- anthropenai.rb
+++ anthropenai.rb
@@ -1,9 +1,8 @@
 class Anthropenai < Formula
   desc "Anthropic-to-OpenAI Proxy"
   homepage "https://github.com/ibehnam/anthropenai"
-  url "https://github.com/ibehnam/anthropenai/archive/refs/heads/main.tar.gz"
+  url "https://github.com/ibehnam/anthropenai.git", branch: "main"
   version "1.0.0"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
 
   depends_on "node"
 
